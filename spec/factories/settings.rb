adc3359c0ac2870ebf559eacc9e3d9fb8fed54a5
note109 <note109@gmail.com>
2014-04-11 10:41:42 +0900

    fix spec/factories/setting

--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -4,11 +4,7 @@ FactoryGirl.define do
   factory :setting do
     name "MyString"
     value "MyString"
-<<<<<<< HEAD
     valueType "MyString"
-=======
-    value-type "MyString"
->>>>>>> 000baa60a10b38ed1fee8819ef98ff820f165a41
     description "MyString"
   end
 end
