d7ec6541fbfee5536e8a4ee1b1b4db3314b8c5fa
note109 <note109@gmail.com>
2014-04-11 10:39:05 +0900

    settings_controller 記述

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,37 @@
 class SettingsController < ApplicationController
+
+  def index
+    @settings = Setting.all
+    respond_to do |format|
+      format.html
+      format.json {render :json => @settings}
+    end
+  end
+
+  def show
+    @setting = Setting.find(params[:id])
+  end
+
+  def new
+    @setting = Setting.new
+  end
+
+  def create
+    @setting = Setting.new(setting_params)
+    @setting.save
+    redirect_to settings_path
+  end
+
+  def destroy
+    @setting = Setting.find(params[:id])
+    @setting.destroy
+    redirect_to settings_path
+  end
+
+  private
+
+    def setting_params
+      params[:setting].permit(:name, :value, :valueType, :description)
+    end
+
 end
